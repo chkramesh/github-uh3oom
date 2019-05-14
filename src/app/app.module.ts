030295f162a2717d8519c5f5b8a39fdc8c4f72d3
chkramesh <chkramesh@gmail.com>
2019-05-14 14:30:10 -0400

    2 files modified, 13 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,55 @@
-import { GlobalErrorHandler } from './global-error-handler';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from './material/material.module';
+import { Routes, RouterModule } from '@angular/router';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+
+import { GlobalErrorHandler } from './global-error-handler';
+import { ServerErrorInterceptor } from './server-error.interceptor';
+
 
 import { AppComponent } from './app.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { ServerErrorInterceptor } from './server-error.interceptor';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { OthersComponent } from './others/others.component';
+import { ObservablesComponent } from './observables/observables.component';
+import { AddressComponent } from './address/address.component';
+import { MembersComponent } from './members/members.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'others', component: OthersComponent },
+  { path: 'address', component: AddressComponent },
+  { path: 'members', component: MembersComponent },
+  { path: 'observables', component: ObservablesComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    OthersComponent,
+    ObservablesComponent,
+    AddressComponent,    
+    MembersComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatSnackBarModule
+    MaterialModule,
+    MatSnackBarModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
     { provide: ErrorHandler, useClass: GlobalErrorHandler },
